cd39d6079904c22703ef8b6173dee55f0a9f53a9
Rana B <rb@rana.sg>
2021-12-04 14:53:44 +0800

    added new to rana

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Rana </Text>
+      <Text>Hello Rana new </Text>
       <StatusBar style="auto" />
     </View>
   );
